5e1fb7c3b346ef234282698e3203e067f0e18e50
Josh <joshuacolclough2@googlemail.com>
2019-02-07 13:52:24 +0000

    Actually works

--- main.go
+++ main.go
@@ -3,6 +3,8 @@ package main
 import (
   "github.com/gen2brain/raylib-go/raylib"
 	box2d "github.com/neguse/go-box2d-lite/box2dlite"
+	"strconv"
+
 	"planet"
 )
 
@@ -13,28 +15,92 @@ const (
 
 var (
 	World *box2d.World
-	Planets []*planet.Planet
+	done = make(chan bool)
+	paused bool = false
 )
+
+func Draw() {
+	for i := 0; i < len(planet.Planets); i++ {
+		planet.Planets[i].Draw()
+	}
+}
 
 func Update(dt float64) {
 	World.Step(dt)
-	for i := 0; i < len(Planets); i++ {
-		Planets[i].Update()
+	for i := 0; i < len(planet.Planets); i++ {
+		go planet.Planets[i].Update()
 	}
+}
+
+func makeSqr(offsetX, offsetY, r float64, w, h int) {
+	for i := 0; i < w; i++ {
+		for j := 0; j < h; j++ {
+			planet.Planets = append(planet.Planets, planet.NewPlanet(len(planet.Planets), ((float64(i)*r*2)+offsetX), ((float64(j)*r*2)+offsetY), 0, 0, r))
+		}
+	}
+}
+
+
+func checkInputs(mouseDown *bool, startMX, startMY, endMX, endMY *int32) {
+	if rl.IsMouseButtonDown(0) && !(*mouseDown) {
+		*mouseDown = true
+		*startMX, *startMY = rl.GetMouseX(), rl.GetMouseY()
+	}
+
+	if rl.IsMouseButtonReleased(0) {
+		*mouseDown = false
+		*endMX, *endMY = rl.GetMouseX(), rl.GetMouseY()
+		println(*endMX-*startMX, *endMY-*startMY)
+		if rl.IsMouseButtonReleased(0) {
+			planet.Planets = append(planet.Planets, planet.NewPlanet(len(planet.Planets), float64(*startMX), float64(*startMY), float64(*startMX-*endMX), float64(*startMY-*endMY), 5))
+		}
+	}
+
+	if rl.IsKeyPressed(rl.KeyP) {
+		paused = !paused
+	}
+	if rl.IsKeyPressed(rl.KeyR) {
+		reset()
+	}
+}
+
+func reset() {
+	World.Clear()
+	planet.Planets = []*planet.Planet{}
 }
 
 func main() {
   rl.InitWindow(int32(SCREEN_W), int32(SCREEN_H), "Planets")
   rl.SetTargetFPS(60)
 
-	World = box2d.NewWorld(box2d.Vec2{0, 0}, 10)
+	var (
+		mouseDown bool  = false
+		startMX   int32 = 0
+		startMY   int32 = 0
+		endMX     int32 = 0
+		endMY     int32 = 0
+	)
+
+	planet.Planets = []*planet.Planet{}
+	World = box2d.NewWorld(box2d.Vec2{0, 0}, 20)
 	planet.World = World
 
+	//planet.Planets = append(planet.Planets, planet.NewPlanet(int32(len(planet.Planets)), 300, 300, 0, 0, 5))
+	//planet.Planets = append(planet.Planets, planet.NewPlanet(int32(len(planet.Planets)), 300, 400, 0, 0, 5))
+	makeSqr(200, 200, 1, 20, 2)
+
   for !rl.WindowShouldClose() {
-		Update(float64(rl.GetFrameTime()))
+		if !paused {
+			Update(float64(rl.GetFrameTime()))
+		}
+		checkInputs(&mouseDown, &startMX, &startMY, &endMX, &endMY)
 
     rl.BeginDrawing()
 		rl.ClearBackground(rl.Black)
+		Draw()
+
+		rl.DrawFPS(10, 10)
+		rl.DrawText("Body count: "+strconv.Itoa(len(planet.Planets)), 10, 40, 12, rl.RayWhite)
 
 		rl.EndDrawing()
   }
--- src/planet/planet.go
+++ src/planet/planet.go
@@ -5,13 +5,21 @@ import (
 	box2d "github.com/neguse/go-box2d-lite/box2dlite"
 )
 
+const (
+	//G float64 = 0.0000000000667
+	G float64 = 0.000667
+	PL_DENSITY float64 = 5514
+	PL_FRICTION float64 = 0
+)
+
 var (
 	World *box2d.World
+	Planets []*Planet
 )
 
 type Planet struct {
 	box2d.Body
-	ID int32
+	ID int
 	Rad float64
 }
 
@@ -20,18 +28,36 @@ func getMass(d, r float64) float64 {
 	return d*area
 }
 
-func NewPlanet(id int32, x, y, vx, vy, r, d float64) *Planet {
+func NewPlanet(id int, x, y, vx, vy, r float64) *Planet {
 	p := Planet{
 		ID: id,
 		Rad: r,
 	}
-	p.Set(&box2d.Vec2{r*2, r*2}, getMass(d, r))
+	p.Set(&box2d.Vec2{r*2, r*2}, getMass(PL_DENSITY, r))
+	p.Body.Friction = PL_FRICTION
 	p.Body.Position = box2d.Vec2{x, y}
 	p.Body.Velocity = box2d.Vec2{vx, vy}
 	World.AddBody(&p.Body)
 	return &p
 }
 
-func (self *Planet) Update() {
+func (self *Planet) getGravForce() box2d.Vec2 {
+	var fTotal = box2d.Vec2{0, 0}
+	for i := 0; i < len(Planets); i++ {
+		if Planets[i].ID != self.ID {
+			distVec, dist := GetDistance(self.Body.Position, Planets[i].Position)
+			f := G * (float64(self.Body.Mass*Planets[i].Body.Mass) / (dist * dist))
+			fTotal.X += f * distVec.X
+			fTotal.Y += f * distVec.Y
+		}
+	}
+	return fTotal
+}
 
+func (self *Planet) Update() {
+	self.Body.Force = self.getGravForce()
+}
+
+func (self *Planet) Draw() {
+	rl.DrawCircleLines(int32(self.Body.Position.X), int32(self.Body.Position.Y), float32(self.Rad), rl.RayWhite)
 }
